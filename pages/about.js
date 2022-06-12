a7ceb1ff87f581c7faa93eb7f11c5c8fd2935c11
Aske21 <veledarevica@yahoo.com>
2022-06-12 21:29:55 +0200

    Added about us page

--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,59 @@
 import React from "react";
+import {
+  Box,
+  Divider,
+  Heading,
+  Stack,
+  Text,
+  useColorModeValue,
+} from "@chakra-ui/react";
 
 const about = () => {
-  return <div>about</div>;
+  return (
+    <Box py={6} px={5} min={"100vh"}>
+      <Stack spacing={4} width={"100%"} direction={"column"}>
+        <Stack
+          p={5}
+          alignItems={"left"}
+          justifyContent={{
+            base: "flex-start",
+            md: "space-around",
+          }}
+          direction={{
+            base: "column",
+            md: "row",
+          }}
+        ></Stack>
+        <Heading size={"lg"}>Why Black Steel?</Heading>
+
+        <Text>
+          At vero eos et accusamus et iusto odio dignissimos ducimus qui
+          blanditiis praesentium voluptatum deleniti atque corrupti quos dolores
+          et quas molestias excepturi sint occaecati cupiditate non provident,
+          similique sunt in culpa qui officia deserunt mollitia animi, id est
+          laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita
+          distinctio. Nam libero tempore, cum soluta nobis est eligendi optio
+          cumque nihil impedit quo minus id quod maxime placeat facere possimus,
+        </Text>
+        <Divider />
+        <Box align={"center"}>
+          <img src="/about.png" width={"1500vw"} alt="Loading" />
+        </Box>
+        <Divider />
+        <Heading size={"lg"}>Our story</Heading>
+        <Text>
+          At vero eos et accusamus et iusto odio dignissimos ducimus qui
+          blanditiis praesentium voluptatum deleniti atque corrupti quos dolores
+          et quas molestias excepturi sint occaecati cupiditate non provident,
+          similique sunt in culpa qui officia deserunt mollitia animi, id est
+          laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita
+          distinctio. Nam libero tempore, cum soluta nobis est eligendi optio
+          cumque nihil impedit quo minus id quod maxime placeat facere possimus,
+          omnis voluptas assumenda est, omnis dolor repellendus.
+        </Text>
+      </Stack>
+    </Box>
+  );
 };
 
 export default about;
